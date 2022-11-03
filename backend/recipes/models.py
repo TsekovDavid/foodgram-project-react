d8ef703e5a3c5815ac18abb9edad79676e86817b
David Tsekov <davidtsekov@MacBook-Air-David.local>
2022-11-03 19:10:58 +0300

    Ingredient model created

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -3,10 +3,6 @@ from django.db import models
 
 
 class Tag(models.Model):
-    COLOR_PALETTE = [
-        ("#FFFFFF", "white", ),
-        ("#000000", "black", ),
-    ]
     name = models.CharField(
         max_length=200,
         verbose_name='Название',
@@ -22,6 +18,32 @@ class Tag(models.Model):
         unique=True
     )
 
-# slug	
-# string or null <= 200 characters ^[-a-zA-Z0-9_]+$
-# Уникальный слаг
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.slug[:20]
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Ингредиент',
+        db_index=True
+    )
+    measurement_unit = models.CharField(
+        max_length=200,
+        verbose_name='Единица измерения'
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return f'{self.name}, {self.measurement_unit}'
+
+
+class Recipes(models.Model):
+    ingredients = 
