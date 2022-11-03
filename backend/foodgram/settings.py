990e98dca575f075aacb7f2e965f3c45060fac06
David Tsekov <davidtsekov@MacBook-Air-David.local>
2022-11-03 13:50:31 +0300

    Tag model created

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes.apps.RecipesConfig',
+    'users.apps.UsersConfig',
+    'api.apps.ApiConfig',
+    'rest_framework',
+    'django_filters',
+    'colorfield',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,27 @@
+from colorfield.fields import ColorField
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    COLOR_PALETTE = [
+        ("#FFFFFF", "white", ),
+        ("#000000", "black", ),
+    ]
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название',
+        db_index=True
+    )
+    color = ColorField(
+        format='hex',
+        verbose_name='Цвет в HEX'
+    )
+    slug = models.SlugField(
+        max_length=200,
+        verbose_name='Уникальный слаг',
+        unique=True
+    )
+
+# slug	
+# string or null <= 200 characters ^[-a-zA-Z0-9_]+$
+# Уникальный слаг
