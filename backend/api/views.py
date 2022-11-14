7cf51a635f5caf087c3d8aafc8a43c7b37c71cd6
David Tsekov <davidtsekov@MacBook-Air-David.local>
2022-11-15 00:10:27 +0300

    created UsersView

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from djoser.views import UserViewSet
+from rest_framework import status, viewsats
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
+from api.serializers import UsersSerializer, FollowSerializer
+from users.models import Follow, User
 
-# Create your views here.
+
+class UsersViewSet(UserViewSet):
+    queryset = User.objects.all()
+    serializer_class = UsersSerializer
+    permission_classes = (AllowAny,)
+    extra_serializer = FollowSerializer
+
+    @action(detail=False,
+            permission_classes=(IsAuthenticated,))
+    def subscriptions(self, request):
+        user = request.user
+        authors = Follow.objects.filter(user=user)
+        pages = self.paginate_queryset(authors)
+        serializer = self.extra_serializer(
+            pages,
+            many=True,
+            context={'request': request}
+        )
+        return self.get_paginated_response(serializer.data)
+
+    @action(methods=['POST', 'DELETE'], detail=True)
+    def subscribe(self, request, **kwargs):
+        user = request.user
+        author = get_object_or_404(User, id=kwargs.get('id'))
+        if request.method == 'POST':
+            if user == author:
+                return Response({
+                    'errors': 'Нельзя подписаться на самого себя'},
+                    status=status.HTTP_404_BAD_REQUEST
+                )
+            if Follow.objects.filter(user=user, author=author).exists():
+                return Response(
+                    {'errors': 'Уже подписаны'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            follow = Follow.objects.create(user=user, author=author)
+            serializer = self.extra_serializer(
+                follow, context={'request': request}
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if request.method == 'DELETE':
+            if user == author:
+                return Response(
+                    {'errors': ''}
+                )
