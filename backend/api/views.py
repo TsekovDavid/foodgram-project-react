d8ece764eb011e9b3bf0ecab1ba45b6aff1bb453
David Tsekov <davidtsekov@MacBook-Air-David.local>
2022-11-15 16:35:45 +0300

    views complited

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.db.models import Sum
 from djoser.views import UserViewSet
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
-from api.serializers import UsersSerializer, IngredientSerializer, FollowSerializer, TagSerializer
+from api.serializers import UsersSerializer, RecipeSerializer, FavouriteSerializer, IngredientSerializer, FollowSerializer, TagSerializer, CreateRecipeSerializer
 from users.models import Follow, User
-from recipes.models import Tag, Ingredient
-from api.permissions import IsAdminOrReadOnly
+from recipes.models import Tag, Ingredient, Recipe, Favourites, ShoppingList, IngredientInRecipe
+from api.permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 
 
 class UsersViewSet(UserViewSet):
@@ -73,4 +74,89 @@ class IngredientViewSet(viewsets.ModelViewSet):
     serializer_class = IngredientSerializer
     permission_classes = (IsAdminOrReadOnly,)
     filter_backends = (DjangoFilterBackend,)
-    filterset_class = IngredientFilter
+    pagination_class = None
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = CreateRecipeSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+    extra_serializer = FavouriteSerializer
+    filter_backends = (DjangoFilterBackend,)
+
+    def get_serializer_class(self):
+        if self.request.method in ['POST', 'PUT', 'PATCH']:
+            return CreateRecipeSerializer
+        return RecipeSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    @action(detail=True, methods=['POST', 'DELETE'],
+            permission_classes=(IsOwnerOrReadOnly,))
+    def favorite(self, request, **kwargs):
+        if request.method == 'POST':
+            return self.add_recipe(Favourites, request, kwargs.get('pk'))    
+        if request.method == 'DELETE':
+            return self.delete_recipe(Favourites, request, kwargs.get('pk'))
+
+    @action(detail=True, methods=['GET', 'POST', 'DELETE'],
+            permission_classes=(IsAuthenticated,))
+    def shopping_cart(self, request, **kwargs):
+        if request.method == 'POST':
+            return self.add_recipe(ShoppingList, request, kwargs.get('pk'))
+        if request.method == 'DELETE':
+            return self.delete_recipe(ShoppingList, request, kwargs.get('pk'))
+
+    @action(
+        methods=['GET'],
+        detail=False,
+        permission_classes=(IsAuthenticated,)
+    )
+    def download_shopping_cart(self, request):
+        user = self.request.user
+        ingredients = IngredientInRecipe.objects.filter(
+            recipe__shopping_list__user=request.user
+        ).values_list(
+            'ingredient__name',
+            'ingredient__measurement_unit'
+        ).order_by('ingredient__name').annotate(
+            ingredient_sum=Sum('amount')
+        )
+        filename = f'{user.username}_shopping_list.txt'
+        shopping_cart = {}
+        for ingredient in ingredients:
+            name = ingredient[0]
+            shopping_cart[name] = {
+                'amount': ingredient[2],
+                'measurement_unit': ingredient[1]
+            }
+            shopping_list = ['Список покупок\n']
+            for key, value in shopping_cart.items():
+                shopping_list.append(
+                    f'{key}: {value["amount"]} {value["measurement_unit"]}\n'
+                )
+        response = HttpResponse(
+            shopping_list, content_type='text.txt; charset=utf-8'
+        )
+        response['Content-Disposition'] = (
+            f'attachment; filename={filename}.txt'
+        )
+        return response
+
+    def add_recipe(self, model, request, pk):
+        recipe = get_object_or_404(Recipe, id=pk)
+        if model.objects.filter(recipe=recipe, user=request.user).exists():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        instance = model.objects.create(user=request.user, recipe=recipe)
+        serializer = FavouriteSerializer(
+            instance, context={'request': request}
+        )
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+    def delete_recipe(self, model, request, pk):
+        recipe = get_object_or_404(Recipe, id=pk)
+        if model.objects.filter(user=request.user, recipe=recipe).exists():
+            model.objects.filter(user=request.user, recipe=recipe).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
