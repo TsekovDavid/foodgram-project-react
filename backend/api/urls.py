b50e7ec33fe91bf0105d2a7b01e5c4ed246e0c95
TsekovDavid <tsekov.david@mail.ru>
2022-11-18 13:34:23 +0300

    sorted imports

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,6 @@ from rest_framework.routers import DefaultRouter
 
 from .views import IngredientViewSet, RecipeViewSet, TagsViewSet, UsersViewSet
 
-
 app_name = 'api'
 
 router = DefaultRouter()
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,6 +1,6 @@
 from colorfield.fields import ColorField
-from django.core.validators import MinValueValidator
 from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from users.models import User
