fba936a2c4f042cd72615096475e3fd2ded09228
TsekovDavid <tsekov.david@mail.ru>
2022-11-19 16:08:21 +0300

    changed location static

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -137,11 +137,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'recipes/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'recipes/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
