be3997faeeacb8744b60dbe9343923489cb22155
TsekovDavid <tsekov.david@mail.ru>
2022-11-20 02:23:41 +0300

    changed ALLOWES_HOSTS to the only working option

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = ('*')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=['*'])
 
 
 INSTALLED_APPS = [
